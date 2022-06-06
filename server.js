d1fe071c612c2318dd420d86d06b7cf7ebfb07a3
LautaroRolon <lrolon@sysworld.com.ar>
2022-06-05 23:27:11 -0300

    Cuarta subida

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
 const puppeteer = require("puppeteer");
+const express = require('express');
+
+/** */
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+})
+  
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
+
+/** */
 
 const browserP = puppeteer.launch({
 	args: ["--no-sandbox", "--disable-setuid-sandbox"]
@@ -10,6 +26,10 @@ const client = new Client();
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
+
+	app.get('/asd', (req, res) => {
+		res.send(qr);
+	})
 });
 
 client.on('ready', () => {
