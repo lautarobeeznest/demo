609a02c39125bb559f059d91e3f67d1a74a9775e
LautaroRolon <lrolon@sysworld.com.ar>
2022-06-05 22:28:14 -0300

    Primera subida

---
+++ server.js
@@ -0,0 +1,45 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+    message.body = message.body.toLowerCase();
+
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
+
+    if(message.body === 'silva') {
+		message.reply('El gei');
+	}
+
+    if(message.body === 'david') {
+		message.reply('El queu ele');
+	}
+
+    if(message.body === 'ditter') {
+		message.reply('Se la come');
+	}
+
+    if(message.body === 'rocio') {
+		message.reply('Mi bb <3');
+	}
+
+    if(message.body.includes('sumar')) {
+        let n = message.body.split(' ');
+        let respuesta = parseInt(n[1]) + parseInt(n[2]);
+        
+		message.reply(respuesta);
+	}
+});
+
+client.initialize();
